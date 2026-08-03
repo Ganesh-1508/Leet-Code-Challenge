95c4da5251186e7302195cd0db18d7b89c886ff9
Ghodke Ganesh <179398197+Ganesh-1508@users.noreply.github.com>
2026-08-03 16:51:29 +0530

    Time: 0 ms (100%), Space: 13.7 MB (44.04%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,22 +4,19 @@ public:
 
         int low = 0;
         int high = nums.size() - 1;
-
+        int ans=nums.size();
         while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
+        int mid=(low+high)/2;
+        if(nums[mid]>=target)
+        {
+            ans=mid;
+            high=mid-1;
         }
-
-        return low;
+        else 
+        {
+            low=mid+1;
+        }
+        }
+        return ans;
     }
 };
