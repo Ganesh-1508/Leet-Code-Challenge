86119c88af75d57aee5213044fdfa0068bf08bc1
Ghodke Ganesh <179398197+Ganesh-1508@users.noreply.github.com>
2026-08-21 22:05:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.7%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+       if(n==0) return 0;
+
+       int f1=0;
+       int f2=1;
+       
+       for(int i=2;i<=n;i++)
+       {
+         int f3=f1+f2;
+         f1=f2;
+         f2=f3;
+       }
+       return f2;
+    }
+};
